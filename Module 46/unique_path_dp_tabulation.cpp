447960c77b56645b8b188dff2022f2685621efbf
IbrahimBagwan1 <irbagwan12@gmail.com>
2025-07-29 20:21:19 +0530

    updated code

---
+++ Module 46/unique_path_dp_tabulation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) { 
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 1; // Only one way for first row or column
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+int main() {
+    Solution obj;
+    int m, n;
+    cin >> m >> n;   // Input grid dimensions
+    cout << obj.uniquePaths(m, n) << endl;
+    return 0;
+}
